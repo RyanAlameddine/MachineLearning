f50f1d09f773f5bca5ddbd5056a6f22801b06905
Ryan Alameddine <rhalameddine@gmail.com>
2018-10-07 12:32:55 -0700

    Initial Commit

---
+++ HillClimber/HillClimber/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Threading;
+
+namespace HillClimber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string target = Console.ReadLine();
+            Random random = new Random();
+
+            char[] value = new char[target.Length];
+            for(int i = 0; i < target.Length; i++)
+            {
+                value[i] = (char) random.Next(32, 127);
+            }
+
+            float error;
+
+            do
+            {
+                error = Error(value, target);
+                Mutate(error, value, target, random);
+                error = Error(value, target);
+                Console.Write(new string(value) + " => ");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.Write(target);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" (");
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write($"{error:#0.00}");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine(")");
+                Thread.Sleep(10);
+            } while (error > 0);
+
+            Console.ReadKey();
+        }
+
+        static void Mutate(float currentError, char[] value, string target, Random random)
+        {
+            
+            float updatedError;
+            char prevChar;
+            do
+            {
+                int index = random.Next(0, value.Length);
+                prevChar = value[index];
+                if (random.Next(0, 2) > 0)
+                {
+                    value[index] = (char)(prevChar + 1);
+                }
+                else
+                {
+                    value[index] = (char)(prevChar - 1);
+                }
+                updatedError = Error(value, target);
+                if(updatedError > currentError)
+                {
+                    value[index] = prevChar;
+                }
+            } while (updatedError > currentError);
+        }
+
+        static float Error(char[] value, string target)
+        {
+            float error = 0;
+            for(int i = 0; i < target.Length; i++)
+            {
+                error += Math.Abs(value[i] - target[i]);
+            }
+            error /= target.Length;
+
+            return error;
+        }
+    }
+}
---
+++ Perceptron/Perceptron/Perceptron.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Perceptron
+{
+    class Perceptron
+    {
+        double bias; //Threshold
+        double[] weights;
+        Random random;
+
+        public Perceptron(int inputCount, Random random)
+        {
+            this.random = random;
+            weights = new double[inputCount];
+            Randomize();
+        }
+
+        public void Randomize()
+        {
+            //set w0 = 1, w1 = 1
+            //randomize the weights between 0 - 2
+
+            for(int i = 0; i < weights.Length; i++)
+            {
+                weights[i] = random.NextDouble() * 2;
+            }
+        }
+
+        public void Mutate(double scale)
+        {
+            int index = random.Next(0, weights.Length);
+
+        }
+
+        // 1 2 = 3
+        // 5 7 = 13
+        public double MAE(double[][] inputs, double[][] outputs)
+        {
+            double error = 0;
+            for(int i = 0; i < inputs.Length; i++)
+            {
+                for(int j = 0; j < inputs[i].Length; j++)
+                {
+                    error += Math.Abs(outputs[i][j] - inputs[i][j]);
+                }
+            }
+            error /= inputs.Length;
+            return error;
+        }
+
+        public double Compute(double[] inputs)
+        {
+            // z = i1 * w1 + i2 * w2
+            double output = 0;
+
+            for(int i = 0; i < inputs.Length; i++)
+            {
+                output += inputs[i] * weights[i];
+            }
+
+            return output;
+        }
+    }
+}
