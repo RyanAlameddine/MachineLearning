e80d40a0bdb3cce40e64a794d16103d59acd54ec
Ryan Alameddine <rhalameddine@gmail.com>
2019-02-17 12:34:36 -0800

    Added MarkovChain

---
+++ MarkovChain/MarkovChain/MarkovChain.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MarkovChain
+{
+    class MarkovChain
+    {
+        Dictionary<string, MarkovNode> nodes = new Dictionary<string, MarkovNode>();
+        
+        public void ReadText(string text)
+        {
+            nodes.Clear();
+            text = text.Replace("\r\n\r\n", " \n\n");
+            //text = text.ToLower();
+            string[] words = text.Split(' ');
+            nodes.Add(words[0], new MarkovNode(new Dictionary<string, int>()));
+            for(int i = 1; i < words.Length; i++)
+            {
+                if (!nodes.ContainsKey(words[i]))
+                {
+                    nodes.Add(words[i], new MarkovNode(new Dictionary<string, int>()));
+                }
+                if (!nodes[words[i - 1]].weights.ContainsKey(words[i]))
+                {
+                    nodes[words[i - 1]].weights.Add(words[i], 1);
+                }
+                else
+                {
+                    nodes[words[i - 1]].weights[words[i]] += 1;
+                }
+            }
+        }
+
+        public string GetText(string currentWord, string end = "\n\nEPISODECOMPLETE")
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            Random random = new Random();
+
+            stringBuilder.Append(currentWord);
+            stringBuilder.Append(' ');
+
+            MarkovNode currentNode = nodes[currentWord];
+
+            while(currentWord != end)
+            {
+                List<string> wordOptions = new List<string>(currentNode.weights.Count);
+                foreach(string word in currentNode.weights.Keys)
+                {
+                    for(int i = 0; i < currentNode.weights[word]; i++)
+                    {
+                        wordOptions.Add(word);
+                    }
+                }
+
+                int index = random.Next(0, wordOptions.Count);
+                currentWord = wordOptions[index];
+                currentNode = nodes[currentWord];
+                stringBuilder.Append(currentWord);
+                stringBuilder.Append(' ');
+            }
+            
+
+
+            return stringBuilder.ToString();
+        }
+    }
+
+    struct MarkovNode
+    {
+        public Dictionary<string, int> weights;
+
+        public MarkovNode(Dictionary<string, int> weights)
+        {
+            this.weights = weights;
+        }
+    }
+}
---
+++ MarkovChain/MarkovChain/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MarkovChain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //format("episodes.txt");
+
+            Console.WriteLine("Enter path to output:");
+            string name = Console.ReadLine();
+            MarkovChain chain = new MarkovChain();
+            chain.ReadText(File.ReadAllText("genesis.txt"));
+            File.WriteAllText(name, chain.GetText("In", "\n\nENDCHAPTER"));
+        }
+
+        static void format(string path)
+        {
+            string[] lines = File.ReadAllLines(path);
+            for(int i = 0; i < lines.Length; i++)
+            {
+                if(lines[i].Length != 0)
+                {
+                    lines[i] = "[" + lines[i] + "]";
+                }
+            }
+
+            File.WriteAllLines(path, lines);
+
+            string text = File.ReadAllText(path);
+
+            text = Regex.Replace(text, "^[^\n]+:.+", (m) =>
+            {
+                return m.Value.Substring(1, m.Value.Length - 3);
+            }, RegexOptions.Multiline);
+
+            File.WriteAllText(path, text);
+        }
+    }
+}
